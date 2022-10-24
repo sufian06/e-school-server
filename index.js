c164b22a0937c18af0368c91f474934e33394e81
sufian <sufian4se@gmail.com>
2022-10-24 21:50:34 +0600

    make categories data and categories api

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
   res.send('e-school API Running');
 });
+
+app.get('/courses-categories', (req, res) => {
+  res.send(categories);
+})
 
 app.listen(port, () => {
   console.log('education server is running on port:', port);
